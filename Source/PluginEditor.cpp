0c572f3a388cb48b516d5c8908c253d2d9ca69d7
Kyle Whaley <kitikdub@gmail.com>
2023-06-14 17:23:13 -0400

    Inital Commit
    Set up and attached frequency, feedback, and drywet knobs for GUI

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,64 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SimpleDelayAudioProcessorEditor::SimpleDelayAudioProcessorEditor(SimpleDelayAudioProcessor& p)
+    : AudioProcessorEditor(&p), audioProcessor(p), freqAT(audioProcessor.apvts, "freq", freq),
+    feedbackAT(audioProcessor.apvts, "feedback", feedback), dryWetAT(audioProcessor.apvts, "dryWet", dryWet)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    freq.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    freq.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 100, 20);
+    addAndMakeVisible(freq);
+
+    feedback.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    feedback.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 100, 20);
+    addAndMakeVisible(feedback);
+
+    dryWet.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    dryWet.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 100, 20);
+    addAndMakeVisible(dryWet);
+
+    setSize (400, 300);
+}
+
+SimpleDelayAudioProcessorEditor::~SimpleDelayAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void SimpleDelayAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void SimpleDelayAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+
+    auto bounds = getLocalBounds();
+
+    auto freqBounds = bounds.removeFromLeft(bounds.getWidth() * .33);
+    freq.setBounds(freqBounds);
+
+    auto feedbackBounds = bounds.removeFromLeft(bounds.getWidth() * .5);
+    feedback.setBounds(feedbackBounds);
+
+    auto dryWetBounds = bounds.removeFromLeft(bounds.getWidth());
+    dryWet.setBounds(dryWetBounds);
+}
---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,208 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SimpleDelayAudioProcessor::SimpleDelayAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+SimpleDelayAudioProcessor::~SimpleDelayAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String SimpleDelayAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SimpleDelayAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimpleDelayAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimpleDelayAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SimpleDelayAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SimpleDelayAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SimpleDelayAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SimpleDelayAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String SimpleDelayAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SimpleDelayAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void SimpleDelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void SimpleDelayAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SimpleDelayAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SimpleDelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        // ..do something to the data...
+    }
+}
+
+//==============================================================================
+bool SimpleDelayAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* SimpleDelayAudioProcessor::createEditor()
+{
+    return new SimpleDelayAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SimpleDelayAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SimpleDelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout SimpleDelayAudioProcessor::createParameterLayout()
+{
+    using namespace juce;
+
+    AudioProcessorValueTreeState::ParameterLayout layout;
+
+    auto freqRange = NormalisableRange<float>(1, 3000, 1, 1);
+    auto feedbackRange = NormalisableRange<float>(.01, 1, .01, 1);
+    auto dryWetRange = NormalisableRange<float>(.01, 1, .01, 1);
+
+    layout.add(std::make_unique<AudioParameterFloat>("freq", "Frequency", freqRange, 50));
+    layout.add(std::make_unique<AudioParameterFloat>("feedback", "Feedback", feedbackRange, .5));
+    layout.add(std::make_unique<AudioParameterFloat>("dryWet", "Dry/Wet", dryWetRange, .5));
+
+    return layout;
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SimpleDelayAudioProcessor();
+}
